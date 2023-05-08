1fe35c340a6a893d3c131f1e7a7b85cbce4af456
Adebola Omoboya <adeomoboya@gmail.com>
2023-05-08 17:09:54 +0100

    First Commit

---
+++ src/main/java/io/factorialsystems/msscstoreeureka/MsscStoreEurekaApplication.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscstoreeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscStoreEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscStoreEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/factorialsystems/msscstoreeureka/MsscStoreEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.factorialsystems.msscstoreeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscStoreEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
